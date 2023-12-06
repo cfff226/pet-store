a7fc78a922ac51b352ffb5b5ed814d589d022126
cfff226 <cazziednb@outlook.com>
2023-12-06 10:31:04 +0000

    Initial commit

---
+++ pet-store/pet-store.py
@@ -0,0 +1,15 @@
+# A program which allows the user to add, remove and checkout with items that they purchase
+
+# Display the menu and the basket until the user checks out
+
+done = False
+while not done:
+    print("Welcome to Paws n Cart \n\nWhat would you like to do?\n")
+    print("1. Add an item to your cart")
+    print("2. Remove an item from your cart")
+    print("3. View your cart")
+    print("4. Checkout")
+
+
+
+
