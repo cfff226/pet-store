8baa73c04c80534b3e1894273d8286cfb8cd31e3
cfff226 <cazziednb@outlook.com>
2023-12-06 13:43:02 +0000

    Remove while loop from if statement and edit the indentation so the code runs smoothly

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -31,48 +31,45 @@ shopping_cart = []
 # Request input from user for their menu choice and the item that they would like to add to basket
 
 
-while True:
-    if choice == "1":
-        # Menu which shows the user the range of items
-        print("\n1. Purina One Cat Salmon - Whole Grain 3KG\n")
-        print("2. Iams Senior 7+ Cat Food With Ocean Fish 2KG\n")
-        print("3. Iams Adult 1+ Cat Food With Fresh Chicken 800KG\n")
-        print("4. Felix As Good As It Looks Cat Food Mixed Selection 40X100G\n")
-        print("5. Felix Doubley Delicious Cat Food Meaty Selection 12x100G\n")
-        print("6. Winalot Wet Dog Food Pouches Meaty Chunks In Jelly 12X100G\n")
-        print("7. Bakers Whirlers Double Flavour Twisted Treats 130G\n")
-        print("8. Chicken And Country Vegetable Dry Dog Food 800KG\n")
-        add_item = input(
-            "\nPlease input the number of the item that you would like to add to your basket: "
-        )
-        if add_item.lower() == "1":
-                    shopping_cart.append("1. Purina One Cat Salmon - Whole Grain 3KG")
-                    print(shopping_cart)
-                    break
-        elif add_item.lower() == "2":
-                    shopping_cart.append("2. Iams Senior 7+ Cat Food With Ocean Fish 2KG")
-                    print(shopping_cart)
-                    break
+if choice == "1":
+    # Menu which shows the user the range of items
+    print("\n1. Purina One Cat Salmon - Whole Grain 3KG\n")
+    print("2. Iams Senior 7+ Cat Food With Ocean Fish 2KG\n")
+    print("3. Iams Adult 1+ Cat Food With Fresh Chicken 800KG\n")
+    print("4. Felix As Good As It Looks Cat Food Mixed Selection 40X100G\n")
+    print("5. Felix Doubley Delicious Cat Food Meaty Selection 12x100G\n")
+    print("6. Winalot Wet Dog Food Pouches Meaty Chunks In Jelly 12X100G\n")
+    print("7. Bakers Whirlers Double Flavour Twisted Treats 130G\n")
+    print("8. Chicken And Country Vegetable Dry Dog Food 800KG\n")
+    add_item = input(
+        "\nPlease input the number of the item that you would like to add to your basket: "
+    )
+    if add_item.lower() == "1":
+                shopping_cart.append("1. Purina One Cat Salmon - Whole Grain 3KG")
+                print(shopping_cart)
+    elif add_item.lower() == "2":
+                shopping_cart.append("2. Iams Senior 7+ Cat Food With Ocean Fish 2KG")
+                print(shopping_cart)
+        
 
-    elif choice == "2":
-        remove_item = input(
-            "Please input the item that you would like to remove from your basket: "
-        )
-    elif choice == "3":  # This will include a function for the user to view their cart
-        pass
-    elif (
-        choice == "4"
-    ):  # This will exit the program and include a function for the user to checkout with items
-        print("Thank you for shopping with Paws n Cart")
-        done = True
-        break
-    else:
-        break
+elif choice == "2":
+    remove_item = input(
+        "Please input the item that you would like to remove from your basket: "
+    )
+elif choice == "3":  # This will include a function for the user to view their cart
+    pass
+elif (
+    choice == "4"
+):  # This will exit the program and include a function for the user to checkout with items
+    print("Thank you for shopping with Paws n Cart")
+    done = True
+else:
+
 
 
 # Display output to the user to tell them the price of the item
 
-print(f"This item costs ....")
+    print(f"This item costs ....")
 
 # Display output to the user to tell them that the item has been added to their basket
 
