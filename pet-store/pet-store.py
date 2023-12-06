d75e61ae0758826954373f5c28110fa02f04025d
cfff226 <cazziednb@outlook.com>
2023-12-06 11:27:47 +0000

    Move relevent print statement to choice 4 in the if statement - Add a bootlean and set to True if the user selects choice 4 - Add a break to break out of the while loop once the user selects choice 4

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -2,8 +2,8 @@
 
 # Display the menu and the basket until the user checks out
 
-#done = False
-#while (not done):
+# done = False
+# while (not done):
 print("Welcome to Paws n Cart \n\nWhat would you like to do?\n")
 print("1. Add an item to your cart")
 print("2. Remove an item from your cart")
@@ -12,17 +12,27 @@ print("4. Checkout")
 
 # Request input from user for their menu choice and the item that they would like to add to basket
 
-choice = input("\nPlease enter the number of the option that you would like to choose: ")
+choice = input(
+    "\nPlease enter the number of the option that you would like to choose: "
+)
 
 while True:
     if choice == "1":
-        add_item = input("Please input the item that you would like to add to your basket: ")
+        add_item = input(
+            "Please input the item that you would like to add to your basket: "
+        )
     elif choice == "2":
-        remove_item = input("Please input the item that you would like to remove from your basket: ")
-    elif choice == "3": # This will include a function for the user to view their cart
+        remove_item = input(
+            "Please input the item that you would like to remove from your basket: "
+        )
+    elif choice == "3":  # This will include a function for the user to view their cart
         pass
-    elif choice == "4": # This will include a function for the user to checkout with items
-        pass
+    elif (
+        choice == "4"
+    ):  # This will exit the program and include a function for the user to checkout with items
+        print("Thank you for shopping with Paws n Cart")
+        done = True
+        break
     else:
         break
 
@@ -34,12 +44,5 @@ print(f"This item costs ....")
 # Display output to the user to tell them that the item has been added to their basket
 
 print("This item has been added to your basket successfully")
-
-# Break out of the loop if the user selects choice 4 to checkout
-
-if choice == "4":
-    # Exit the program
-    print("Thank you for shopping with Paws n Cart")
-    done = True
 
 # Menu which shows the user the range of items
