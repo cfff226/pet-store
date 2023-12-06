48fb3b45293b9846c1ce1006a7234249d767ed92
cfff226 <cazziednb@outlook.com>
2023-12-06 11:00:04 +0000

    Add while loop to a request further information from user about what items they would like to add or remove from basket

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -2,18 +2,24 @@
 
 # Display the menu and the basket until the user checks out
 
-done = False
-while not done:
-    print("Welcome to Paws n Cart \n\nWhat would you like to do?\n")
-    print("1. Add an item to your cart")
-    print("2. Remove an item from your cart")
-    print("3. View your cart")
-    print("4. Checkout")
+#done = False
+#while (not done):
+print("Welcome to Paws n Cart \n\nWhat would you like to do?\n")
+print("1. Add an item to your cart")
+print("2. Remove an item from your cart")
+print("3. View your cart")
+print("4. Checkout")
 
 # Request input from user for their menu choice and the item that they would like to add to basket
 
-choice = input("Please enter the number of the option that you would like to choose: ")
-item = input("Please input the item that you would like to add to your basket: ")
+choice = input("\nPlease enter the number of the option that you would like to choose: ")
+
+while True:
+    if choice == "1":
+        add_item = input("Please input the item that you would like to add to your basket: ")
+    elif choice == "2":
+        remove_item = input("Please input the item that you would like to remove from your basket: ")
+
 
 # Display output to the user to tell them the price of the item
 
@@ -22,6 +28,8 @@ print(f"This item costs ....")
 # Display output to the user to tell them that the item has been added to their basket
 
 print("This item has been added to your basket successfully")
+
+# Break out of the loop if the user selects choice 4 to checkout
 
 if choice == "4":
     # Exit the program
