b7cb927fb03db731bc9120a61c1bb2721c85f50b
cfff226 <cazziednb@outlook.com>
2023-12-11 12:34:20 +0000

    Add spaces in menu items so that each list item contains the same amount of chars

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -8,9 +8,9 @@ shopping_cart = []
 shopping_quant = []
 price_total = []
 
-menu = ["Purina One Cat Salmon - Whole Grain", "Iams Senior 7+ Cat Food With Ocean Fish", "Iams Adult 1+ Cat Food With Fresh Chicken",
-"Felix As Good As It Looks Cat Food Mixed Selection", "Felix Doubley Delicious Cat Food Meaty Selection", "Winalot Wet Dog Food Pouches Meaty Chunks In Jelly",
- "Bakers Whirlers Double Flavour Twisted Treats", "Chicken And Country Vegetable Dry Dog Food"]
+menu = ["Purina One Cat Salmon - Whole Grain               ", "Iams Senior 7+ Cat Food With Ocean Fish           ", "Iams Adult 1+ Cat Food With Fresh Chicken         ",
+"Felix As Good As It Looks Cat Food Mixed Selection", "Felix Doubley Delicious Cat Food Meaty Selection  ", "Winalot Wet Dog Food Pouches Meaty Chunks In Jelly",
+ "Bakers Whirlers Double Flavour Twisted Treats     ", "Chicken And Country Vegetable Dry Dog Food        "]
 
 prices = [7.99, 5.99, 3.99, 7.00, 4.00, 3.99, 4.00, 6.99]
 
